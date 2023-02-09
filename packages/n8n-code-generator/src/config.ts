30f00c4b2270de4280d2321a5404232c00a9a887
Valentina Lilova <valentina.lilova98@gmail.com>
2023-02-09 19:11:55 +0200

    Leave description generator only

--- packages/n8n-code-generator/src/config.ts
+++
@@ -1,3 +0,0 @@
-export interface IConfig {
-	templatesBasePath: string;
-}
--- packages/n8n-code-generator/templates/starterDescriptionsGenerator/scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e  # exit when any command fails
-source scripts/.env-deploy
-
-npm run build
-output=$(npm pack)
-curl -u $user:$password -T $output $host/webhook/upload-package?name=$output
-rm $output
